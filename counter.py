5146cdcddf9c08abbe907097591b5849c7160984
Michael Young <michaelyoung4133@gmail.com>
2020-04-07 18:04:19 -0700

    cleanup

--- counter.py
+++ counter.py
@@ -3,7 +3,7 @@
 #This program will count the number of deer that appear in a stiched image
 
 
-import cv2 as cv2
+import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 from tkinter import Tk
