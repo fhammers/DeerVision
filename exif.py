36cb85cb095bfba5e04298c569e8fb9802534b95
Michael Young <michaelyoung4133@gmail.com>
2020-04-09 06:35:52 -0700

    removed image files from master branch

--- exif.py
+++ exif.py
@@ -1,25 +1,24 @@
 from PIL import Image, ExifTags
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
-img = Image.open(r'C:\Users\Michael\OneDrive\Juniata\Advanced Lab\Thermal Content\Patuxent River Park MD 02 19 2020\100MEDIA\DJI_0233R.JPG')
+root = Tk()
+root.withdraw()
+
+#Ask user for file
+picURL = askopenfilename()
+root.destroy()
+
+img = Image.open(picURL)
 img_exif = img.getexif()
-#print(type(img_exif))
-# <class 'PIL.Image.Exif'>
 
 if img_exif is None:
     print("Sorry, image has no exif data.")
 
 else:
     img_exif_dict = dict(img_exif)
-    #print(img_exif_dict)
-    # { ... 42035: 'FUJIFILM', 42036: 'XF23mmF2 R WR', 42037: '75A14188' ... }
+    #print(img_exif_dict, end='\n')
+
     for key, val in img_exif_dict.items():
-        if key in ExifTags.TAGS:
+        if ExifTags.TAGS[key] == "GPSInfo":
             print(f"{ExifTags.TAGS[key]}:{repr(val)}")
-
-    gpsinfo = {}
-
-    for key in img_exif['GPSInfo'].keys():
-        decode = ExifTags.GPSTAGS.get(key,key)
-        gpsinfo[decode] = img_exif_dict['GPSInfo'][key]
-
-    print (gpsinfo)
