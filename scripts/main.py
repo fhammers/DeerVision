fbb593026fcf3df90439ad420588fb61eb85bb4f
bktotient@gmail.com <bktotient@gmail.com>
2020-04-22 13:08:18 -0400

    add main.py functions

--- scripts/main.py
+++ scripts/main.py
@@ -1,36 +1,116 @@
 ## main execution script, first run through
 from tkinter import *
+import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
+from webodmAPI import WebODMAPI
+import time
+
+import os
+
+status_codes = {
+    "QUEUED": 10,
+    "RUNNING": 20,
+    "FAILED" : 30,
+    "COMPLETED": 40,
+    "CANCELED": 50
+}
 
 
-
-
-
-
-
-class DeerVision:
+class DeerVision(Tk):
     def __init__(self):
+        super(DeerVision, self).__init__()
          
         self.list_dir =  ""
-
-
+        
+        print(os.getcwd())
         #initialize tkinter window
-        self.window = Tk()
-        self.window.title("Deer Vision")
-        self.window.geometry('600x200')
-        btn = Button(self.window, text="Browse", command = self.load_file, width = 10).pack()
+        self.title("Deer Vision")
+        self.minsize(640, 400)
+        # self.configure(background='grey')
+       
 
+        self.labelFrame = ttk.LabelFrame(self, text="Select a directory")
+        self.labelFrame.grid(column = 0, row = 1, padx = 20, pady = 20)
+        self.labelFrame.pack(padx=10, pady=20)
+        self.text = None
 
-    def start(self):
-        self.window.mainloop()
+        self.button()
+        self.text = tk.Text(self.labelFrame, height=2, width = 30)
+        self.text.grid(column = 1, row = 4)
 
-
+        self.odm_API = WebODMAPI()
+        # btn = Button(self.window, text="Browse", command = self.load_webODM, width = 10).pack()
     
+    def button(self, text = ""):
+        self.button = ttk.Button(self.labelFrame, text = "Browse A File", command=self.load_file)
+        self.button.grid(column = 1, row =1)
+
+        self.sbt_button = ttk.Button(self.labelFrame, text= "Submit", command=self.load_webODM)
+        self.sbt_button.grid(column = 1, row= 3)
+        
+        
+
     def load_file(self):
-        file_name = filedialog.askdirectory()
+        file_name = filedialog.askdirectory(initialdir="./")
+        self.label = ttk.Label(self.labelFrame, text = "")
+        self.label.grid(column = 1, row = 2)
+        self.label.configure(text = file_name)
 
         if file_name:
             self.list_dir = file_name
+        else:
+            raise ValueError("Invalid file directory")
+
+        return self.list_dir
+
+   
+    def load_webODM(self):
+
+        auth = self.odm_API.authenticate()
+        
+        if not auth:
+            # handle lack of authentication
+            return 
+        
+        self.odm_API.load_images(self.list_dir)
+
+        #stitch images
+        task_id = self.odm_API.stitch_images()
+
+        #get progress
+        if task_id:
+            print("hello")
+            self.get_status(task_id)
+
+        self.odm_API.download_tif(task_id)
+
+    def get_status(self, task_id):
+
+        while True:
+            self.text.delete(1.0,END)
+            res = self.odm_API.get_stitch_status(task_id)
+            if res['status'] == status_codes["COMPLETED"]:
+                print("Task has completed!")
+                self.text.insert(tk.END, "Task has completed!")
+                break
+            elif res['status'] == status_codes["FAILED"]:
+                print("Task failed: {}".format(res))
+                self.text.insert(tk.END, "Task failed: {}".format(res))
+                sys.exit(1)
+            elif res['status'] == status_codes["QUEUED"]:
+                print("Task has been qeued")
+                self.text.insert(tk.END, "Task has been qeued")
+                time.sleep(3)
+            else:
+                print("Processing, hold on...")
+                self.text.insert(tk.END, "Processing, hold on...")
+                time.sleep(3)
+            self.update_idletasks()
+
+
+
+        #start file processing
 
 
 
@@ -44,10 +124,9 @@ def load_file():
 
 if __name__ == "__main__":
     deer_process = DeerVision()
+    deer_process.mainloop()
 
-    
-
-    deer_process.start()
+    # deer_process.start()
 
 
 
